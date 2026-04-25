15ddc5eeb832e29e5e792d742065c6517a60ae3d
Arjan-Chaudhary <arjan25csu385@ncuindia.edu>
2026-04-25 14:32:04 +0530

    Create Help Alice.cpp

---
+++ Help Alice.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long l, r;
+    cin >> l >> r;
+    
+    // Count of odd numbers in [l, r]
+    long long oddCount = (r + 1) / 2 - l / 2;
+    
+    if(oddCount % 2 == 1)
+        cout << "odd" << endl;
+    else
+        cout << "even" << endl;
+    
+    return 0;
+}
